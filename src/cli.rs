297573f006d7aa59b5f2ee32ccab0496695cdbe5
Will Wolf <williamabrwolf@gmail.com>
2022-09-13 14:07:54 -0400

    write simulation results to .csv; remove cli flag to visualize result

--- src/cli.rs
+++ src/cli.rs
@@ -6,6 +6,6 @@ pub struct Cli {
     pub runs: u32,
     #[clap(long = "steps_per_run")]
     pub steps_per_run: u32,
-    #[clap(long = "viz_results")]
-    pub viz_results: bool,
+    #[clap(long = "output_path")]
+    pub output_path: String,
 }
--- src/simulation.rs
+++ src/simulation.rs
@@ -1,3 +1,4 @@
+use csv;
 use std::marker::PhantomData;
 
 use crate::{state::BaseState, system::BaseSystem};
@@ -19,7 +20,34 @@ impl<U: BaseState, T: BaseSystem<U>> Simulation<U, T> {
         &self,
         runs: u32,
         steps_per_run: u32,
-        _visualize_results: bool,
+        output_path: String,
+    ) -> Result<(), csv::Error> {
+        let results = self._run(runs, steps_per_run);
+        let mut writer = csv::Writer::from_path(output_path)?;
+        if let Some((state, _run, _step)) = results.first() {
+            let mut keys = vec!["run", "step"];
+            // Get data columns; assumed to be the same for all records.
+            let cols = state.get_serializable_record().keys().cloned().collect::<Vec<&str>>();
+            keys.extend(cols);
+            // Write .csv header.
+            writer.write_record(&keys)?;
+            // Append simulation results.
+            for (state, run, step) in results.iter() {
+                let mut record = state.get_serializable_record();
+                record.insert("run", (*run).into());
+                record.insert("step", (*step).into());
+                let vals = keys.iter().map(|k| record.get(*k).unwrap().to_string());
+                writer.write_record(vals)?;
+            }
+        }
+        writer.flush()?;
+        Ok(())
+    }
+
+    fn _run(
+        &self,
+        runs: u32,
+        steps_per_run: u32
     ) -> Vec<(U, u32, u32)> {
         let mut results = Vec::<(U, u32, u32)>::new();
         for run in 0..runs {
