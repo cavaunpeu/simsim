1c873fa703fd572ba4e6f49f6fe6f4d3d61859e4
Will Wolf <williamabrwolf@gmail.com>
2022-09-16 16:48:28 -0400

    add lorenz example

---
+++ examples/lorenz/src/main.rs
@@ -0,0 +1,15 @@
+pub mod system;
+pub mod state;
+
+use clap::Parser;
+use std::error::Error;
+use simsim::{cli::Cli, util::load_configs, simulation::Simulation};
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::parse();
+    let configs = load_configs(args.configs_path)?;
+    let simulation = Simulation::<state::State, system::LorenzSystem>::from_configs(configs);
+    simulation.run(args.runs, args.steps_per_run, args.output_dir)?;
+    Ok(())
+}
