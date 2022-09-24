0aa2fcf809d8c3d22dceba0cf88cd8a6380c396b
Will Wolf <williamabrwolf@gmail.com>
2022-09-24 12:35:13 -0400

    update examples to conform to updated api

--- examples/lorenz/src/state.rs
+++ examples/lorenz/src/state.rs
@@ -1,19 +1,8 @@
-use std::collections::HashMap;
-use simsim::state;
+use serde::Serialize;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub struct State {
   pub x: f64,
   pub y: f64,
   pub z: f64,
-}
-
-impl state::BaseState for State {
-    fn get_serializable_record(&self) -> HashMap<&str, f64> {
-        HashMap::from([
-            ("x", self.x),
-            ("y", self.y),
-            ("z", self.z),
-        ])
-    }
 }
--- examples/lotka_volterra/src/state.rs
+++ examples/lotka_volterra/src/state.rs
@@ -1,17 +1,7 @@
-use std::collections::HashMap;
-use simsim::state;
+use serde::Serialize;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub struct State {
   pub population_size: f64,
   pub food_supply: f64,
-}
-
-impl state::BaseState for State {
-    fn get_serializable_record(&self) -> HashMap<&str, f64> {
-        HashMap::from([
-            ("population_size", self.population_size),
-            ("food_supply", self.food_supply),
-        ])
-    }
 }
